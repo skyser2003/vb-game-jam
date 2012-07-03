d23f2ea8cfc65e7f87a4b23ed0070bcdb894d770
Seung Ho Jang <shjang1992@hotmail.com>
2012-07-03 21:37:25 +0900

    init player.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import os, pygame
+from util import *
+from pygame.locals import *
+from pygame.compat import geterror
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((700,450))
+    pygame.display.set_caption("Dinosaur Run")
+    pygame.mouse.set_visible(0)
+
+    #background = pygame.image.load("images/background.gif")
+    
+    #screen.blit(background, (0,0))
+
+    running = True
+
+    #pygame.display.update()
+    while False:
+        True
+    pygame.quit()
+
+if __name__ == '__main__':
+        main()
