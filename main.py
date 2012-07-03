086116468663b5d021ca68eadf7a383e67c6a62f
Jae Joon <Jae Joon@JaeJoon-PC.local>
2012-07-04 00:54:25 +0900

    py

--- main.py
+++ main.py
@@ -7,21 +7,56 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((700,450))
     pygame.display.set_caption("Dinosaur Run")
-    pygame.mouse.set_visible(0)
+    
+    background = pygame.image.load("images/background.png")
+    redColor = pygame.Color(255,0,0)
+    #screen.blit(background, (0,0))
+    
+
     clock = pygame.time.Clock()
-    counter = 0
 
-    background = pygame.image.load("/Users/FrozenGuy/Pictures/Raichu.png")
+    running = True
+    state = 0
 
-    while True:
-        screen.blit(background,(0,0))
-        clock.tick(60)
-        pygame.display.update()
-        counter += 1
-
-        if(counter == 300):
-            break
+    font = pygame.font.Font(None, 18)
+    largeFont = pygame.font.Font(None, 30)
+    
+    
+    while running:
         
+
+        if state == 0:
+            screen.blit(pygame.image.load("images/start.png"),(0,0))
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    state = 1
+                    
+        
+        elif state == 1:
+            screen.blit(background, (0,0))
+            for event in pygame.event.get():
+                    if event.type == QUIT:
+                        running = False
+                    elif event.type == KEYDOWN:
+                        running = False
+                    elif event.type == MOUSEBUTTONUP:
+                        mousex, mousey = event.pos
+                        print (mousex,mousey)
+        elif state == 2:
+            pygame.draw.polygon(screen,redColor,
+                                (146,0),(291,106),(236,277),(56,277),(0,106))
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    state = 0
+        elif state == 3:
+            pygame.draw.line(screen,redColor,(60,160),(120,60),4)
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    state = 1
+        
+            
+        pygame.display.update()
+    
     pygame.quit()
 
 if __name__ == '__main__':
