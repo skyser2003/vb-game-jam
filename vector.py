fcfab1156ddc6b5c6d59d9ce99440e08d5f3b0df
Seung Ho Jang <shjang1992@hotmail.com>
2012-07-04 22:59:02 +0900

    modified??

---
+++ vector.py
@@ -0,0 +1,13 @@
+class Vec2:
+    x = None
+    y = None
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def set(x,y):
+        self.x = x
+        self.y = y
+    def setX(x):
+        self.x = x
+    def setY(y):
+        self.y = y
