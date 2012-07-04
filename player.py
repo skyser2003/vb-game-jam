5c0863b9b0b891d613f260f45b880dadc161d2db
Jae Joon <Jae Joon@JaeJoon-PC.local>
2012-07-04 20:48:07 +0900

    Backed out merge changeset: 5252129339fc
    Backed out merge revision to its second parent (348b1f39cba9)

--- player.py
+++ player.py
@@ -1,20 +1,27 @@
-from character import Character
+from vector import Vec2
 
-class Player(Character):
-    __damageReduceSpeed = 10
-    __isDamaged = False
-    __damageProgressTime = 0
+class Player:
+    __acceleration = Vec2(0,0)
+    __speed = Vec2(0,0)
+    __position = Vec2(0,0)
+    __isDamaged = 0
     
-    def getAttacked(self):
-        self.__speed.x -= self.__damageReduceSpeed
-        self.__isDamaged = True
-        self.__damageProgressTime = 0
-    def recoverFromAttack(self):
-        self.__speed.x += self.__damageReduceSpeed
-        self.__isDamaged = False
-        self.__damageProgressTime = 0
-    def update(self,fps):
-        Character.update(self,fps)
-        
-        if(self.__isDamaged == True):
-            self.__damageProgressTime += 60/fps
+    def __init__(self):
+        self.__speed.x = 1
+        self.__speed.y = 0
+    def setSpeed(self,speedX,speedY):
+        self.__speed.x = speedX
+        self.__speed.y = speedY
+    def setSpeed(self,speed):
+        self.__speed = speed
+    def setSpeedX(self,speedX):
+        self.__speed.x = speedX
+    def setSpeedY(self,speedY):
+        self.__speed.y = speedY
+    def setPosition(self,position):
+        self.__position = position
+    def update(self):
+        self.__speed.x += self.__acceleration.x
+        self.__speed.y += self.__acceleration.y
+        self.__position.x += self.__speed.x
+        self.__position.y += self.__speed.y
