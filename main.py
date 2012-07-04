2a5eca9bcfb965860967627f1ea7c3d5b36bb35c
Jae Joon <Jae Joon@JaeJoon-PC.local>
2012-07-05 00:20:45 +0900

    dino imported

--- main.py
+++ main.py
@@ -34,10 +34,16 @@ def main():
     white = pygame.Color(255,255,255)
     black = pygame.Color(0,0,0)
 
+    cloud = pygame.image.load("images/cloud.png")
+
     player1 = pygame.image.load("images/player1.png")
     player2 = pygame.image.load("images/player2.png")
     player3 = pygame.image.load("images/player3.png")
     player4 = pygame.image.load("images/player4.png")
+    dino1 = pygame.image.load("images/dino1.png")
+    dino2 = pygame.image.load("images/dino2.png")
+    dino3 = pygame.image.load("images/dino3.png")
+    dino4 = pygame.image.load("images/dino4.png")
 
     #Create clock
     clock = pygame.time.Clock()
@@ -70,8 +76,9 @@ def main():
     timer = 0
     rotateTimer = 0
     playerx = 300
-    addx = 1
-    
+    dinox = 15
+    addPlayerx = 1
+    addDinox = 1
     while running:
         screen.blit(sky, (0,0))
          
@@ -101,9 +108,6 @@ def main():
                     if button3.isInner(coord) == True:
                         running = False
 
-                #Game playing 
-                if event.type == KEYDOWN:
-                    state = 1
 
         elif state == 1:
             #rolling background
@@ -111,53 +115,72 @@ def main():
             screen.blit(background,(rotateTimer+700,0))
             rotateTimer -= 1
             if rotateTimer == -700:
-                rotateTimer = 0
-
-<<<<<<< local
-           
-=======
->>>>>>> other
-            #player animation        
+                rotateTimer =0
+            #player+dino animation        
             if timer%15 == 0:
                 screen.blit(player1, (playerx,260))
+                screen.blit(dino1, (dinox,300))
             elif timer%15 == 1:
                 screen.blit(player1, (playerx,260))
+                screen.blit(dino1, (dinox,300))
             elif timer%15 == 2:
                 screen.blit(player2, (playerx,260))
+                screen.blit(dino2, (dinox,300))
             elif timer%15 == 3:
                 screen.blit(player2, (playerx,260))
+                screen.blit(dino2, (dinox,300))
             elif timer%15 == 4:
                 screen.blit(player3, (playerx,260))
+                screen.blit(dino3, (dinox,300))
             elif timer%15 == 5:
                 screen.blit(player3, (playerx,260))
+                screen.blit(dino3, (dinox,300))
             elif timer%15 == 6:
                 screen.blit(player3, (playerx,260))
+                screen.blit(dino3, (dinox,300))
             elif timer%15 == 7:
                 screen.blit(player4, (playerx,260))
+                screen.blit(dino4, (dinox,300))
             elif timer%15 == 8:
                 screen.blit(player4, (playerx,260))
+                screen.blit(dino4, (dinox,300))
             elif timer%15 == 9:
                 screen.blit(player4, (playerx,260))
+                screen.blit(dino4, (dinox,300))
             elif timer%15 == 10:
                 screen.blit(player3, (playerx,260))
+                screen.blit(dino3, (dinox,300))
             elif timer%15 == 11:
                 screen.blit(player3, (playerx,260))
+                screen.blit(dino3, (dinox,300))
             elif timer%15 == 12:
                 screen.blit(player2, (playerx,260))
+                screen.blit(dino3, (dinox,300))
             elif timer%15 == 13:
                 screen.blit(player2, (playerx,260))
+                screen.blit(dino2, (dinox,300))
             elif timer%15 == 14:
                 screen.blit(player1, (playerx,260))
+                screen.blit(dino1, (dinox,300))
             #moving player back and forth
             if playerx == 280:
-                addx = 1
+                addPlayerx = 1
             if playerx == 320:
-                addx = -1
-            
-            playerx += addx
-
+                addPlayerx = -1
+            #moving dino back and forth
+            if dinox == 10:
+                addDinox = 1
+            if dinox == 40:
+                addDinox = -1
+                
+            playerx += addPlayerx
+            dinox += addDinox
             timer += 1
+
+            screen.blit(cloud, (0,0))
+
             #questions
+
 
             if(questionOn == False):
                 questionOffTimer += 1
@@ -228,7 +251,6 @@ def main():
                     elif event.type == MOUSEBUTTONUP:
                         mousex, mousey = event.pos
 
-                        print (mousex,mousey)
         
         #?? second button in main menu
 
@@ -242,7 +264,7 @@ def main():
                 if event.type == MOUSEBUTTONUP:
                     mousex, mousey = event.pos
                     coord = Vec2(mousex,mousey)
-                    print (mousex,mousey)
+
                     if button4.isInner(coord) == True:
                         state = 0
             
@@ -259,11 +281,12 @@ def main():
                 if event.type == MOUSEBUTTONUP:
                     mousex, mousey = event.pos
                     coord = Vec2(mousex,mousey)
-                    print (mousex,mousey)
+
                     if button5.isInner(coord) == True:
                         running = False
                     if button6.isInner(coord) == True:
                         state = 0
+                        
         
         clock.tick(100)
         pygame.display.update()
